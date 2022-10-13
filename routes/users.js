3d4966ff0f7591f6fe37938e82b2ae92e681a962
MSY1126 <110950990+MSY1126@users.noreply.github.com>
2022-10-13 15:39:27 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource 민세윤');
 });
 
 module.exports = router;
